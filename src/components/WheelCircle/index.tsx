1c20216ed4348c0c803b4e9a978d4c10f7a3c2c3
Cameron Davis <cam170@icloud.com>
2024-05-16 13:32:05 -0400

    added rotate out animations

--- src/components/WheelCircle/index.tsx
+++ src/components/WheelCircle/index.tsx
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WheelCircle.css";
 
 const WheelCircle = () => {
-  // const floaters = 3;
+  const floaters = 3;
+
+  // create a variable that tracks if the floater is being hovered over
+  const [hover, setHover] = useState(false);
+
+  const handleHover = () => {
+    setHover(!hover);
+  };
 
   return (
+    // body for the floating elements to work within
     <div className="wheel-body">
-      <div className="wheel-circle" />
-      {/* {Array.from({ length: floaters }).map((_, i) => (
-        <div key={i} className="floater" />
-      ))} */}
-      <div className="floater" />
+      {/* element that triggers animations on float */}
+      <div
+        className="wheel-circle"
+        onMouseEnter={handleHover}
+        onMouseLeave={handleHover}
+      />
+      {Array.from({ length: floaters }).map((_, i) => {
+        let ratio = i / floaters;
+        let angle = ratio * 360;
+        return (
+          // Apply 'animate' class when hover is true
+          <div
+            key={i}
+            className={`floater ${hover ? "animate" : ""}`}
+            style={{ "--rotate": `${angle}deg` } as React.CSSProperties}
+          >
+            <p>test</p>
+          </div>
+        );
+      })}
+      {/* <div className="floater" /> */}
+      {/* visual representationof the circle */}
       <div className="inner-circle" />
     </div>
   );
