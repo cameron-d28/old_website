79ab7640ad0e70ae5c545ceeae0b1a5b75f5375b
Cameron Davis <cam170@icloud.com>
2024-05-25 13:13:16 -0700

    change routing and added test.html

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,31 @@ import "./App.css";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Resume from "./pages/resume";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+// function App() {
+//   const path = window.location.pathname;
+
+//   switch (path) {
+//     case "/about":
+//       return <About />;
+//     case "/resume":
+//       return <Resume />;
+//     default:
+//       return <Home />;
+//   }
+// }
 
 function App() {
-  const path = window.location.pathname;
-
-  switch (path) {
-    case "/about":
-      return <About />;
-    case "/resume":
-      return <Resume />;
-    default:
-      return <Home />;
-  }
+  return (
+    <Router>
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
