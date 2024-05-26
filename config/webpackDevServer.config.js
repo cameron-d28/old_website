9b21d1abc40186bfddab57d037e367d66bab7491
Cameron Davis <cam170@icloud.com>
2024-05-25 18:23:40 -0700

    multiple pages in dev

--- config/webpackDevServer.config.js
+++ config/webpackDevServer.config.js
@@ -98,7 +98,7 @@ module.exports = function (proxy, allowedHost) {
       // See https://github.com/facebook/create-react-app/issues/387.
       disableDotRule: true,
       index: paths.publicUrlOrPath,
-      // rewrites: [{ from: /^\/about/, to: "/about.html" }],
+      rewrites: [{ from: /^\/about/, to: "/about.html" }],
     },
     // `proxy` is run between `before` and `after` `webpack-dev-server` hooks
     proxy,
