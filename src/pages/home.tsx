9d05cc15dbe9560d664e51e1d95c11fd915bbdb9
Cameron Davis <cam170@icloud.com>
2024-05-26 13:20:43 -0700

    updated links

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,10 +4,21 @@ import WheelCircle from "../components/WheelCircle";
 import { HoverTypes } from "../components/WheelCircle/types";
 
 const Home = () => {
-  const links = [
+  const about_links = [
     { link: "/about", title: "about" },
     { link: "/resume", title: "Resume" },
     { link: "https://github.com/cameron-d28", title: "github" },
+  ];
+
+  const learn_links = [
+    { link: "https://press.stripe.com/", title: "stripe press" },
+    { link: "https://www.acquired.fm/", title: "acquired podcast" },
+  ];
+
+  const future_links = [
+    { link: "/", title: "coming soon" },
+    { link: "/", title: "coming soon" },
+    { link: "/", title: "coming soon" },
   ];
 
   return (
@@ -18,9 +29,9 @@ const Home = () => {
         <p>carpe diem</p>
       </header>
       <div className="navigation-body">
-        <WheelCircle hovertype={HoverTypes.straight} links={links} />
-        <WheelCircle hovertype={HoverTypes.straight} links={links} />
-        <WheelCircle hovertype={HoverTypes.straight} links={links} />
+        <WheelCircle hovertype={HoverTypes.straight} links={about_links} />
+        <WheelCircle hovertype={HoverTypes.straight} links={learn_links} />
+        <WheelCircle hovertype={HoverTypes.spiral} links={future_links} />
       </div>
     </div>
   );
